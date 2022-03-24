949d11c7afc80648e648df9c8a53be2174c1321d
hammadsi <80900720+hammadsi@users.noreply.github.com>
2022-03-24 19:18:42 +0100

    Translated to english

--- DBinfoScript.py
+++ DBinfoScript.py
@@ -7,7 +7,7 @@ cursor = con.cursor()
 cursor.execute(
     """
     INSERT INTO user (full_name, email, password)
-    VALUES ('Sensor Sensorsen', 'Bruker@email.no', 'BrukerPassord')
+    VALUES ('Bruker', 'Bruker@email.no', 'BrukerPassord')
     """
 )
 cursor.execute(
@@ -66,12 +66,6 @@ cursor.execute(
     VALUES ('cafe de abuela', 88, 'South America', 'Argentina')
     """
 )
-cursor.execute(
-    """
-    INSERT INTO coffee_farm (name, meters_above_sea, region, country)
-    VALUES ('Lola', 93, 'South America', 'Colombia')
-    """
-)
 
 cursor.execute(
     """
@@ -82,7 +76,7 @@ cursor.execute(
 cursor.execute(
     """
     INSERT INTO processing_type (name, description)
-    VALUES ('plain', 'raw ecological coffee, floral')
+    VALUES ('plain', 'raw ecological coffee')
     """
 )
 cursor.execute(
@@ -91,85 +85,60 @@ cursor.execute(
     VALUES ('washed', 'washed, processed, finest quality')
     """
 )
-cursor.execute(
-    """
-    INSERT INTO processing_type (name, description)
-    VALUES ('honey', 'honey processed, for a sweet coffee')
-    """
-)
 
 cursor.execute(
     """
     INSERT INTO coffee_batch (harvest_year, price_pr_kg_usd, processing_type_id, coffee_farm_id)
-    VALUES ('2018', 1, 1, 1)
+    VALUES ('2018-01-01', 1, 1, 1)
     """
 )
 cursor.execute(
     """
     INSERT INTO coffee_batch (harvest_year, price_pr_kg_usd, processing_type_id, coffee_farm_id)
-    VALUES ('2020', 2, 2, 2)
+    VALUES ('2020-02-02', 2, 2, 2)
     """
 )
 cursor.execute(
     """
     INSERT INTO coffee_batch (harvest_year, price_pr_kg_usd, processing_type_id, coffee_farm_id)
-    VALUES ('2022', 3, 3, 3)
-    """
-)
-
-cursor.execute(
-    """
-    INSERT INTO coffee_batch (harvest_year, price_pr_kg_usd, processing_type_id, coffee_farm_id)
-    VALUES ('2022', 3, 4, 4)
+    VALUES ('2022-03-03',3 , 3, 3)
     """
 )
 
 cursor.execute(
     """
     INSERT INTO coffee (roastery, roast_degree,roast_date, name, description, price_per_kg_nok, coffee_batch_id)
-    VALUES ('Tromso-brenneri', 'Light', '2018-01-01', 'TromsoKaffen', 'Floral', 40, 1)
+    VALUES ('Tromso-roastery', 'degree-1', '2018-01-01', 'TromsoCoffee', 'Best served cold', 1, 1)
     """
 )
 cursor.execute(
     """
     INSERT INTO coffee (roastery, roast_degree, roast_date, name, description, price_per_kg_nok, coffee_batch_id)
-    VALUES ('Trondheims-brenneri', 'Cinnamon', '2020-02-02', 'TrondheimsKaffen', 'Setter seg fast i barten', 30, 2)
+    VALUES ('Trondheim-roastery', 'degree-2', '2020-02-02', 'TrondheimCoffee', 'Sticks to the moustache', 1, 2)
     """
 )
 cursor.execute(
     """
     INSERT INTO coffee (roastery, roast_degree, roast_date, name, description, price_per_kg_nok, coffee_batch_id)
-    VALUES ('Oslo-brenneri', 'Dark', '2022-03-03', 'OsloKaffen', 'Alle har meninger om den', 23, 3)
-    """
-)
-cursor.execute(
-    """
-    INSERT INTO coffee (roastery, roast_degree, roast_date, name, description, price_per_kg_nok, coffee_batch_id)
-    VALUES ('Nidaros-brenneri', 'Medium', '2022-02-03', 'NidarosCoffee', 'Sweet, but sour', 20, 4)
+    VALUES ('Oslo-roastery', 'degree-3', '2022-03-03', 'OsloCoffee', 'Everyone has got an opinion', 1, 3)
     """
 )
 cursor.execute(
     """
     INSERT INTO coffee_tasting (notes, score, taste_date, coffee_id, user_id)
-    VALUES ('Dette er kaffe1', 1, '2022-01-01', 1, 1)
+    VALUES ('This is coffee 1', 1, '2018-01-01', 1, 1)
     """
 )
 cursor.execute(
     """
     INSERT INTO coffee_tasting (notes, score, taste_date, coffee_id, user_id)
-    VALUES ('Dette er kaffe2', 1, '2022-03-01', 2, 1)
+    VALUES ('This is coffee 2', 2, '2020-02-02', 2, 2)
     """
 )
 cursor.execute(
     """
     INSERT INTO coffee_tasting (notes, score, taste_date, coffee_id, user_id)
-    VALUES ('Dette er kaffe2', 2, '2022-02-02', 2, 2)
-    """
-)
-cursor.execute(
-    """
-    INSERT INTO coffee_tasting (notes, score, taste_date, coffee_id, user_id)
-    VALUES ('Dette er kaffe 3', 3, '2020-03-03', 3, 3)
+    VALUES ('This is coffee 3', 3, '2022-03-03', 3, 3)
     """
 )
 
