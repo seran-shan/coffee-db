72b49821c52ca53407db75591ed8d89cdc307e34
Seran Shanmugathas <Seran@live.no>
2022-03-25 12:34:55 +0100

    :art: Improving structure

--- DBinfoScript.py
+++ DBinfoScript.py
@@ -66,6 +66,12 @@ cursor.execute(
     VALUES ('cafe de abuela', 88, 'South America', 'Argentina')
     """
 )
+cursor.execute(
+    """
+    INSERT INTO coffee_farm (name, meters_above_sea, region, country)
+    VALUES ('lola', 93, 'South America', 'Colombia')
+    """
+)
 
 cursor.execute(
     """
@@ -89,58 +95,78 @@ cursor.execute(
 cursor.execute(
     """
     INSERT INTO coffee_batch (harvest_year, price_pr_kg_usd, processing_type_id, coffee_farm_id)
-    VALUES ('2018-01-01', 1, 1, 1)
+    VALUES ('2018', 1, 1, 1)
     """
 )
 cursor.execute(
     """
     INSERT INTO coffee_batch (harvest_year, price_pr_kg_usd, processing_type_id, coffee_farm_id)
-    VALUES ('2020-02-02', 2, 2, 2)
+    VALUES ('2020', 2, 2, 2)
     """
 )
 cursor.execute(
     """
     INSERT INTO coffee_batch (harvest_year, price_pr_kg_usd, processing_type_id, coffee_farm_id)
-    VALUES ('2022-03-03',3 , 3, 3)
+    VALUES ('2022',3 , 3, 3)
+    """
+)
+cursor.execute(
+    """
+    INSERT INTO coffee_batch (harvest_year, price_pr_kg_usd, processing_type_id, coffee_farm_id)
+    VALUES ('2022', 3, 4, 4)
     """
 )
 
 cursor.execute(
     """
     INSERT INTO coffee (roastery, roast_degree,roast_date, name, description, price_per_kg_nok, coffee_batch_id)
-    VALUES ('Tromso-roastery', 'degree-1', '2018-01-01', 'TromsoCoffee', 'Best served cold', 1, 1)
+    VALUES ('Tromso-roastery', 'Medium', '2018-01-01', 'TromsoCoffee', 'Best served cold, floral', 1, 1)
     """
 )
 cursor.execute(
     """
     INSERT INTO coffee (roastery, roast_degree, roast_date, name, description, price_per_kg_nok, coffee_batch_id)
-    VALUES ('Trondheim-roastery', 'degree-2', '2020-02-02', 'TrondheimCoffee', 'Sticks to the moustache', 1, 2)
+    VALUES ('Trondheim-roastery', 'Light', '2020-02-02', 'TrondheimCoffee', 'Sticks to the moustache', 1, 2)
     """
 )
 cursor.execute(
     """
     INSERT INTO coffee (roastery, roast_degree, roast_date, name, description, price_per_kg_nok, coffee_batch_id)
-    VALUES ('Oslo-roastery', 'degree-3', '2022-03-03', 'OsloCoffee', 'Everyone has got an opinion', 1, 3)
+    VALUES ('Oslo-roastery', 'Dark', '2022-03-03', 'OsloCoffee', 'Everyone has got an opinion', 1, 3)
+    """
+)
+cursor.execute(
+    """
+    INSERT INTO coffee (roastery, roast_degree, roast_date, name, description, price_per_kg_nok, coffee_batch_id)
+    VALUES ('Nidaros-brenneri', 'Medium', '2022-02-03', 'NidarosCoffee', 'Sweet, but sour', 20, 4)
+    """
+)
+
+cursor.execute(
+    """
+    INSERT INTO coffee_tasting (notes, score, taste_date, coffee_id, user_id)
+    VALUES ('This is coffee 1', 44, '2022-01-01', 1, 1)
     """
 )
 cursor.execute(
     """
     INSERT INTO coffee_tasting (notes, score, taste_date, coffee_id, user_id)
-    VALUES ('This is coffee 1', 1, '2018-01-01', 1, 1)
+    VALUES ('This is coffee 2', 92, '2022-02-02', 2, 1)
     """
 )
 cursor.execute(
     """
     INSERT INTO coffee_tasting (notes, score, taste_date, coffee_id, user_id)
-    VALUES ('This is coffee 2', 2, '2020-02-02', 2, 2)
+    VALUES ('This is coffee 3', 77, '2022-03-03', 2, 2)
     """
 )
 cursor.execute(
     """
     INSERT INTO coffee_tasting (notes, score, taste_date, coffee_id, user_id)
-    VALUES ('This is coffee 3', 3, '2022-03-03', 3, 3)
+    VALUES ('This is coffee 4', 86, '2020-03-03', 3, 3)
     """
 )
+
 
 con.commit()
 con.close()
